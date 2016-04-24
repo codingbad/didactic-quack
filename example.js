5cfacda7e5f9d384b7dccca04927bf6f1b5b4b53
Chu Hoang Son <chuson1996@gmail.com>
2016-04-24 04:10:16 +0300

    Alchemy integration

--- example.js
+++ example.js
@@ -1,21 +1,57 @@
-import { DQ } from './DQ.js';
-import config from './config/conf.js';
-import logger from 'intel';
+// import { DQ } from './DQ.js';
+// import config from './config/conf.js';
+// import logger from 'intel';
 
-const dq = new DQ({
-	token: config.token,
-	parent: config.parent
+import watson from 'watson-developer-cloud';
+import fs from 'fs';
+import request from 'request';
+
+import credentials from './credentials.json';
+import makePromise from './lib/make-promise.js';
+require('babel-polyfill');
+
+const alchemy_vision = watson.alchemy_vision({
+	api_key: credentials.alchemy.apikey
 });
 
-dq.on('message', (message) => {
-	const to = message.to;
-	const text = message.text;
-	const moduleResponse = dq.initModule(text);
+const alchemy_vision_Pr = makePromise(
+	alchemy_vision.recognizeFaces,
+	alchemy_vision);
 
-	dq.send({ to, text: moduleResponse });
-})
+(async () => {
 
+	await new Promise((resolve, reject) => {
+		request('https://scontent-arn2-1.xx.fbcdn.net/v/t34.0-12/13084333_1779439385621466_1503497067_n.jpg?oh=ea6c2e7255dce395184768194e6bb0e3&oe=571E2340')
+			.pipe(fs.createWriteStream('temp.png'))
+			.on('close', (response) => { resolve(response); })
+			.on('error', (error) => { reject(error); });
+	});
 
-dq.listen((err) => {
-  if (err) logger.error(err);
+	let alchemyResult = await alchemy_vision_Pr({
+		image: fs.createReadStream('temp.png')
+	});
+
+	console.log('alchemyResult: ');
+	const { age, gender } = alchemyResult.imageFaces[0];
+	console.log(age, gender);
+})().catch((error) => {
+	console.error(error);
 });
+
+// const dq = new DQ({
+// 	token: config.token,
+// 	parent: config.parent
+// });
+
+// dq.on('message', (message) => {
+// 	const to = message.to;
+// 	const text = message.text;
+// 	const moduleResponse = dq.initModule(text);
+
+// 	dq.send({ to, text: moduleResponse });
+// })
+
+
+// dq.listen((err) => {
+// 	if (err) logger.error(err);
+// });
